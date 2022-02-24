61222cc7a7b113672436ce4fa39d770306cf824a
efilion <eric.l.filion@gmail.com>
2022-02-23 22:02:47 -0800

    Chessboard where black plays random moves

--- react-web-frontend/src/App.test.tsx
+++ react-web-frontend/src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { container } = render(<App />);
+  const whites_king_square = container.querySelector('[data-square=e1]');
+  expect(whites_king_square).toBeInTheDocument();
 });
--- react-web-frontend/src/App.tsx
+++ react-web-frontend/src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ChessJS, { ChessInstance, Square } from 'chess.js';
+import { Chessboard } from 'react-chessboard';
+
+const Chess = (typeof ChessJS === 'object')? ChessJS.Chess : ChessJS;
 
 function App() {
+
+  const [game, setGame] = useState<ChessInstance>(new Chess())
+
+  function safeGameMutate(modify: (g: ChessInstance) => void) {
+    setGame((g) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
+  }
+
+  function makeRandomMove() {
+    const possibleMoves = game.moves();
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
+      return;
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    safeGameMutate((game) => {
+      game.move(possibleMoves[randomIndex])
+    });
+  }
+
+  function onDrop(sourceSquare: Square, targetSquare: Square) {
+    let move = null;
+    safeGameMutate((game) => {
+      move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q"
+      });
+    });
+    if (move === null) return false;
+    setTimeout(makeRandomMove, 200);
+    return true;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chessboard position={game.fen()} onPieceDrop={onDrop} />
     </div>
   );
 }
