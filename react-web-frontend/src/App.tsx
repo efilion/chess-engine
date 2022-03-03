cc9600ae10c5b07b3c2df28173683c112c559ca2
Eric Filion <eric.l.filion@gmail.com>
2022-03-03 15:20:50 -0800

    Frontend calling python service in dev environment

--- react-web-frontend/src/App.tsx
+++ react-web-frontend/src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Axios from 'axios';
 import './App.css';
 import Chessboard from './chessboard/Chessboard';
 
 function App() {
 
+  const [msg, setMsg] = useState("");
+
+  Axios.get('http://' + (process.env.REACT_APP_ENGINE_SERVICE as string))
+    .then((res) => {
+      setMsg(() => res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   return (
-    <div className="App">
-      <Chessboard />
-    </div>
+    <>
+      <p>{msg}</p>
+      <div className="App">
+        <Chessboard />
+      </div>
+    </>
   );
 }
 
