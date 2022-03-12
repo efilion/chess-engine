f0c1e6d05d079ddad7473ee975da4ac7cf6d525e
Eric Filion <eric.l.filion@gmail.com>
2022-03-11 23:15:43 -0800

    Compute moves on python server

--- python/webapi/app/config/staging.py
+++ python/webapi/app/config/staging.py
@@ -1,7 +1,9 @@
 from os import getenv, path
 from dotenv import load_dotenv
+from distutils.util import strtobool
 
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env.staging'))
 
-REACT_HOST=getenv('REACT_HOST')
+REACT_HOST=str(getenv('REACT_HOST'))
+TLS=bool(strtobool(getenv('TLS')))
--- react-web-frontend/src/App.tsx
+++ react-web-frontend/src/App.tsx
@@ -5,19 +5,8 @@ import Chessboard from './chessboard/Chessboard';
 
 function App() {
 
-  const [msg, setMsg] = useState("");
-
-  Axios.get('https://' + (process.env.REACT_APP_ENGINE_SERVICE as string))
-    .then((res) => {
-      setMsg(() => res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
   return (
     <>
-      <p>{msg}</p>
       <div className="App">
         <Chessboard />
       </div>
