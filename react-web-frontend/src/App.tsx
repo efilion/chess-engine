e5eda5fa1f93e7821350b5d5173f625170ef69c9
Eric Filion <eric.l.filion@gmail.com>
2022-08-16 13:10:59 -0700

    Add AppBar and Footer

--- react-web-frontend/src/App.tsx
+++ react-web-frontend/src/App.tsx
@@ -2,7 +2,16 @@ import React, { useCallback, useState } from 'react';
 import './App.css';
 import Chessboard, { BoardProps, GameResult } from './chessboard/Chessboard';
 import { getRandomMove } from './engine/Random';
-import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
+import { AppBar, Box, Toolbar, IconButton, Typography, Container, Divider, Grid,
+  Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions
+} from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+import TypeScript_Logo from './assets/typescript_logo.png';
+import React_Logo from './assets/react_logo.png';
+import Python_Logo from './assets/python_logo.png';
+import Flask_Logo from './assets/flask_logo.png';
 
 function App(props: {boardProps?: Partial<BoardProps>}) {
 
@@ -48,41 +57,92 @@ function App(props: {boardProps?: Partial<BoardProps>}) {
 
   return (
     <>
-      <div className="App">
-        <Chessboard {...boardProps } />
-      </div>
-      <Dialog // Start Dialog
-          open={openStartDialog}
-          aria-labelledby='start-dialog-title'
-      >
-          <DialogTitle id='start-dialog-title'>
-              {'Choose side'}
-          </DialogTitle>
-          <DialogActions>
-              <Button onClick={chooseWhite}>
+    <Box position="relative" minHeight="100vh">
+      <Box>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="h6" sx={{ flexGrow: 1 }}>
+              Chess Engine
+            </Typography>
+            <IconButton aria-label='github.com/efilion/chess-engine'
+              color='inherit'
+              onClick={() => window.open('https://www.github.com/efilion/chess-engine')} 
+            >
+              <GitHubIcon fontSize='large' />
+            </IconButton> 
+            <IconButton aria-label='linkedin.com/in/ericlfilion'
+              color='inherit'
+              onClick={() => window.open('https://www.linkedin.com/in/ericlfilion/')} 
+            >
+              <LinkedInIcon fontSize='large' />
+            </IconButton> 
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box>
+        <Container maxWidth="sm" sx={{ margin: '4vh auto' }}>
+          <div className="App">
+            <Chessboard {...boardProps} />
+            <Dialog // Start Dialog
+              open={openStartDialog}
+              aria-labelledby='start-dialog-title'
+            >
+              <DialogTitle id='start-dialog-title'>
+                {'Choose side'}
+              </DialogTitle>
+              <DialogActions>
+                <Button onClick={chooseWhite}>
                   White
-              </Button>
-              
-              <Button onClick={chooseBlack}>
-                  Black
-              </Button>
-          </DialogActions>
-      </Dialog>
+                </Button>
 
-      <Dialog // End Dialog
-          open={openEndDialog}
-          aria-labelledby='end-dialog-title'
-          aria-describedby='end-dialog-description'
-      >
-          <DialogTitle id='end-dialog-title'>
-              {'Game Over'}
-          </DialogTitle>
-          <DialogContent>
-              <DialogContentText id='end-dialog-description'>
-                { endDialogText }
-              </DialogContentText>
-          </DialogContent>
-      </Dialog>
+                <Button onClick={chooseBlack}>
+                  Black
+                </Button>
+              </DialogActions>
+            </Dialog>
+
+            <Dialog // End Dialog
+              open={openEndDialog}
+              aria-labelledby='end-dialog-title'
+              aria-describedby='end-dialog-description'
+            >
+              <DialogTitle id='end-dialog-title'>
+                {'Game Over'}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id='end-dialog-description'>
+                  {endDialogText}
+                </DialogContentText>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </Container> 
+      </Box>
+      <Box>
+        <Divider />
+        <Container>
+          <Grid maxWidth="sm" container sx={{ margin: '1em auto' }} rowSpacing={2}>
+            <Grid item sm={12}>
+              <Typography variant='h5'>
+                Built with
+              </Typography>
+            </Grid>
+            <Grid item sm={3}>
+              <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"><img src={TypeScript_Logo} className="Logo" alt="TypeScript" /></a>
+            </Grid>
+            <Grid item sm={3}>
+              <a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img src={React_Logo} className="Logo" alt="React" /></a>
+            </Grid>
+            <Grid item sm={3}>
+              <a href="https://www.python.org/" target="_blank" rel="noreferrer"><img src={Python_Logo} className="Logo" alt="Python" /></a>
+            </Grid>
+            <Grid item sm={3}>
+              <a href="https://flask.palletsprojects.com/" target="_blank" rel="noreferrer"><img src={Flask_Logo} className="Logo" alt="Flask" /></a>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </Box>
     </>
   );
 }
