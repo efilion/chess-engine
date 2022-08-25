38b8ba13be52e40a640d2f1d0b1c33ba09714976
Eric Filion <eric.l.filion@gmail.com>
2022-08-24 20:32:58 -0700

    Negamax refactoring, quiescence, and 3-ply depth

--- python/webapi/app/engine/akimbo.py
+++ python/webapi/app/engine/akimbo.py
@@ -1,26 +1,117 @@
 import chess
 import math
+from enum import Enum
+from functools import total_ordering
+from collections import defaultdict
 from app.engine import piece_position_tables
 
 class Akimbo:
 
-    def __init__(self, fen):
+    def __init__(self, fen, default_lookahead=1):
         self.board = chess.Board(fen)
+        self.default_lookahead = default_lookahead
+        self.negamax_memo = defaultdict(lambda: [-1, None])
 
     def recommend_moves(self, format: str):
-        possibleMoves = list(self.board.legal_moves)
+        mates = [move for move in self.board.legal_moves
+            if self.heuristic_score(move) == Score.Checkmate
+        ]
+        if mates:
+            return self.format(mates, format == "san")
+
         whites_turn = self.board.turn == chess.WHITE
-        (_, bestMoves) = self.maxes(possibleMoves,
-            key = lambda: self.minimax(1, whites_turn)[0 if whites_turn else 1]
+        possible_moves = list(sorted(self.board.legal_moves,
+            key=lambda m: self.heuristic_score(m)
+        ))
+
+        self.board.push(possible_moves[0])
+        best_eval, best_moves = -self.negamax(not whites_turn), []
+        self.board.pop()
+
+        for move in possible_moves:
+            self.board.push(move)
+            eval = -self.negamax(not whites_turn, beta=-best_eval)
+            self.board.pop()
+            if eval > best_eval:
+                best_eval, best_moves = eval, [move]
+            elif eval == best_eval:
+                best_moves.append(move)
+
+        return self.format(best_moves, format == "san")
+
+    def heuristic_score(self, move):
+        self.board.push(move)
+        if self.board.is_checkmate():
+            self.board.pop()
+            return Score.Checkmate
+        self.board.pop()
+        if self.board.gives_check(move):
+            return Score.Check
+        elif self.board.is_capture(move):
+            return Score.Capture
+        else:
+            return Score.Else
+
+    def negamax(self, maximizing_player, lookahead_depth=2, alpha=(-math.inf), beta=(math.inf)):
+        
+        fen = self.board.fen()[:-4]
+        update_memo = False
+        [prev_lookahead, prev_score] = self.negamax_memo[fen]
+        if prev_lookahead >= lookahead_depth:
+            return prev_score
+        else:
+            update_memo = True
+
+        if lookahead_depth == 0 or self.board.is_game_over():
+            return self.quiescence(maximizing_player, alpha, beta)
+
+        max_eval = -math.inf
+        possible_moves = sorted(self.board.legal_moves,
+            key=lambda m: self.heuristic_score(m)
         )
-        return [self.board.san(m) if format == "san" else m.uci() for m in bestMoves]
+        for m in possible_moves:
+            self.board.push(m)
+            score = -self.negamax(not maximizing_player, lookahead_depth - 1, -beta, -alpha)
+            self.board.pop()
+            max_eval = max(max_eval, score)
+            alpha = max(alpha, max_eval)
+            if alpha > beta:
+                break
+
+        if update_memo:
+            self.negamax_memo[fen] = [lookahead_depth, max_eval]
+        return max_eval
+
+    def quiescence(self, maximizing_player, alpha, beta):
+        stand_pat = self.evaluate() if maximizing_player else -self.evaluate()
+        if stand_pat >= beta:
+            return stand_pat
+        if stand_pat > alpha:
+            alpha = stand_pat
+        
+        best_move = stand_pat
+        threatening_moves = [move for move in self.board.legal_moves
+            if self.board.is_capture(move) or self.board.gives_check(move)
+        ]
+        for m in threatening_moves:
+            self.board.push(m)
+            score = -self.quiescence(not maximizing_player, -beta, -alpha)
+            self.board.pop()
+
+            best_move = max(best_move, score)
+            alpha = max(alpha, best_move)
+
+            if best_move >= beta:
+                return best_move
+
+        return best_move
 
     def evaluate(self):
-        whitesTurn = self.board.turn == chess.WHITE
+        whites_turn = self.board.turn == chess.WHITE
         if self.board.is_checkmate():
-            eval = [-math.inf, math.inf] if whitesTurn else [math.inf, -math.inf]
+            eval = -math.inf if whites_turn else math.inf
         elif self.drawn():
-            eval = [0, 0]
+            eval = 0
         else:
             whites_score = 0
             blacks_score = 0
@@ -31,47 +122,9 @@ class Akimbo:
                 else:
                     blacks_score += self.material_value(piece)
                     blacks_score += self.position_value(piece, square)
-            eval = [
-                whites_score - blacks_score,
-                blacks_score - whites_score
-            ]
+            eval = whites_score - blacks_score
 
         return eval
-
-    def minimax(self, lookahead_depth, whites_turn):
-
-        if lookahead_depth == 0 or self.board.is_game_over():
-            return self.evaluate()
-
-        maxEval = [-math.inf, -math.inf]
-        for m in self.board.legal_moves:
-            self.board.push(m)
-            maxEval = max(maxEval, self.minimax(lookahead_depth - 1, not whites_turn),
-                key=lambda e: e[1 if whites_turn else 0])
-            self.board.pop()
-        
-        return maxEval
-
-    def drawn(self):
-        is_drawn = self.board.is_stalemate() \
-            or self.board.is_insufficient_material() \
-            or self.board.is_repetition() \
-            or self.board.is_fifty_moves()
-        return is_drawn
-
-    def maxes(self, a, key):
-        self.board.push(a[0])
-        m, max_list = key(), []
-        self.board.pop()
-        for s in a:
-            self.board.push(s)
-            k = key()
-            if k > m:
-                m, max_list = k, [s]
-            elif k == m:
-                max_list.append(s)
-            self.board.pop()
-        return m, max_list
     
     def material_value(self, piece):
         piece_values = {
@@ -99,3 +152,27 @@ class Akimbo:
         }
 
         return position_values[piece.piece_type][rank][file]
+
+    def drawn(self):
+        is_drawn = self.board.is_stalemate() \
+            or self.board.is_insufficient_material() \
+            #or self.board.is_repetition() \
+            #or self.board.is_fifty_moves()
+        return is_drawn
+
+    def format(self, moves, to_san):
+        return [self.board.san(m) if to_san else m.uci() for m in moves]
+
+@total_ordering
+class Score(Enum):
+    Checkmate = 0
+    Check = 1
+    Capture = 2
+    Threat = 3
+    Else = 10
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        else:
+            return NotImplemented
--- python/webapi/test/unit/test_akimbo.py
+++ python/webapi/test/unit/test_akimbo.py
@@ -1,6 +1,7 @@
 from app.engine.akimbo import Akimbo
 from unittest import TestCase
 import chess
+import math
 
 class AkimboPlaysGoodMoves(TestCase):
 
@@ -11,6 +12,28 @@ class AkimboPlaysGoodMoves(TestCase):
     def test_capture_queen(self):
         position = "4r1k1/5pp1/7p/8/8/4R2P/4qPP1/6K1 w - - 0 1"
         self.assertEqual(["Rxe2"], Akimbo(position).recommend_moves("san"))
+
+    def test_doesnt_hang_queen(self):
+        position = "r2qkb1r/1b3ppp/1pn1p3/pBp1Q3/8/2N5/PPPPNPPP/R1B2RK1 b kq - 0 1"
+        recommended_moves = Akimbo(position).recommend_moves("san")
+        self.assertNotIn('Qc7', recommended_moves)
+        self.assertNotIn('Qd5', recommended_moves)
+        self.assertNotIn('Qg5', recommended_moves)
+
+    def test_doesnt_blunder_rook_or_bishop(self):
+        position = "2rk1b1r/5ppp/Np2p3/p1pbQ3/8/2N5/PPPP1PPP/R1B2RK1 b - - 0 1"
+        recommended_moves = Akimbo(position).recommend_moves("san")
+        self.assertNotIn('g5', recommended_moves)
+        self.assertNotIn('Bf3', recommended_moves)
+
+    def test_alekjnies_defence(self):
+        position = "rnbqkb1r/ppp1pppp/5n2/3p4/4P3/2NP4/PPP2PPP/R1BQKBNR b KQkq - 0 3"
+        recommended_moves = Akimbo(position).recommend_moves("san")
+
+    def test_capture_queen_g4(self):
+        position = "4r1k1/5pp1/7p/8/6P1/4R2P/4qP2/6K1 b - - 0 1"
+        recommended_moves = Akimbo(position, 1).recommend_moves("san")
+        pass
 
     def test_scholars_mate_attack(self):
         position = "r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR w KQkq - 4 4"
@@ -23,7 +46,7 @@ class AkimboPlaysGoodMoves(TestCase):
         for move in recommended_moves:
             board.push_san(move)
             legal_moves = [board.san(m) for m in board.legal_moves]
-            self.assertNotIn("Qxf7#", legal_moves)
+            self.assertNotIn("Qxf7#", legal_moves, msg = f'Recommended move {move} led to Qxf7#')
             board.pop()
 
     def test_knight_doesnt_capture_guarded_pawn(self):
@@ -35,8 +58,22 @@ class AkimboEvaluates(TestCase):
 
     def test_scandinavian_defence(self):
         position = "rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2"
-        self.assertEqual([0, 0], Akimbo(position).evaluate())
+        self.assertEqual(0, Akimbo(position).evaluate())
 
     def test_nimzowitsch_defence(self):
         position = "r1bqkbnr/pppppppp/2n5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 1 2"
-        self.assertEqual([-10, 10], Akimbo(position).evaluate())
+        self.assertEqual(-10, Akimbo(position).evaluate())
+
+    def test_captures_unguarded_pawn(self):
+        position = "1k6/2p1p3/1q6/8/8/4Q3/5P2/6K1 w - - 0 1"
+        engine = Akimbo(position)
+        best_move, best_score = None, -math.inf
+        for move in engine.board.legal_moves:
+            engine.board.push(move)
+            score = -engine.quiescence(False, -math.inf, math.inf)
+            engine.board.pop()
+            if score > best_score:
+                best_score = score
+                best_move = move
+        best_move = engine.board.san(best_move)
+        self.assertEqual('Qxe7', best_move)
